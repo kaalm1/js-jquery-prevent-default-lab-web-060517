b36b7f7eb1b31b269172573f70d0ca5a687cf601
kaalm1 <kaalm1@gmail.com>
2017-07-20 14:47:33 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
   // call functions here
+
+submitForm()
+
 });
 
 // define functions here
+
+function submitForm(){
+  $('form').on('submit', function(event){
+
+    debugger
+    var item = $('#item').val()
+    newElem = document.createElement('li')
+    newText  = document.createTextNode(`${item}`)
+    newElem.appendChild(newText)
+    $('ol')[0].appendChild(newElem)
+
+    event.preventDefault()
+
+  })
+}
